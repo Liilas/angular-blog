87659ad15fb733d8f25460e0ea0212497a550c41
Lidi <lidilisboa9@gmail.com>
2023-10-10 14:19:02 -0300

    content created

---
+++ src/app/data/dataFake.ts
@@ -0,0 +1,26 @@
+export const dataFake = [
+  {
+    "id":"1",
+    "title": "Feel Like You're Not Alone, But You Are? Science Can Explain That Creepy Feeling",
+    "description":"If you've ever had the eerie sensation there's a presence in the room when you were sure you were alone, you may be reluctant to admit it. font:https://www.sciencealert.com/feel-like-youre-not-alone-but-you-are-science-can-explain-that-creepy-feeling",
+    "photoCover": "https://www.sciencealert.com/images/2023/04/ShadowyPresenceBehindGlassDoor.jpg"
+  },
+  {
+    "id":"2",
+    "title": "How an objective measure of pain could counter bias in medicine",
+    "description":"font:https://www.bbc.com/future/article/20230414-the-search-for-an-objective-measure-of-pain",
+    "photoCover": "https://ychef.files.bbci.co.uk/1600x900/p0fgp23r.webp"
+  },
+  {
+    "id":"3",
+    "title": "he surprising ways cannabis can be part of drug addiction treatment",
+    "description":"font:https://www.newscientist.com/article/2395129-the-surprising-ways-cannabis-can-be-part-of-drug-addiction-treatment/",
+    "photoCover": "https://d1csarkz8obe9u.cloudfront.net/posterpreviews/health-logo-design-template-413edd6c579e1c7ac61e14ffdd75eec5_screen.jpg?ts=1604410182"
+  },
+  {
+    "id":"4",
+    "title": "The world's most polluted capital city",
+    "description":"font:https://www.bbc.com/future/article/20220405-the-fungi-cleaning-new-delhis-air",
+    "photoCover": "https://ychef.files.bbci.co.uk/1600x900/p0bzhr4s.webp"
+  }
+]
